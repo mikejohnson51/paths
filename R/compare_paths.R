0b4640469a8157f43db78ba1655bd19dc8ca3bd1
mike <jmj00@ucsb.edu>
2019-11-08 19:12:08 -0800

    update compare paths

--- R/compare_paths.R
+++ R/compare_paths.R
@@ -8,21 +8,42 @@
 #' @importFrom sf st_distance st_buffer st_coordinates st_linestring st_sfc st_sf st_set_crs st_crs st_difference st_intersection st_polygon st_zm st_union
 #' @importFrom dplyr filter
 #' @importFrom units set_units
+#'
+
 
 compare_paths = function(gps, same_paths, truth){
 
   all = list()
   truth = st_transform(truth, st_crs(gps))
 
+  empty <- st_as_sfc("POINT(EMPTY)")
+
+  rate = gps %>% mutate(
+    elapsed_time = lead(time) - time,
+    distance_to_next = as.numeric(sf::st_distance(
+      geometry,
+      lead(geometry, default = empty),
+      by_element = TRUE)),
+      rate = distance_to_next/as.numeric(elapsed_time))
+
+  rem = which(rate$rate > 2)
+  rem = c(rem-1,  rem, rem+1)
+  rem2 = which((rate$distance_to_next > 25))
+  rem3 = unique(c(rem,rem2))
+  g = gps[-rem3,]
+
   for(p in 1:nrow(same_paths)){
 
     tmp = same_paths[p, ]
 
-    l1 = gps[gps$path == tmp$Path1,]
-    l2 = gps[gps$path == tmp$Path2,]
+    l1 = g[g$path == tmp$Path1,]
+    l2 = g[g$path == tmp$Path2,]
 
     trueStart = truth[truth$location == tmp$start,]
     trueEnd   = truth[truth$location == tmp$end,]
+
+    l1 = l1[sort(c(which.min(st_distance(trueStart, l1)):which.min(st_distance(trueEnd, l1)))),]
+    l2 = l2[sort(c(which.min(st_distance(trueStart, l2)):which.min(st_distance(trueEnd, l2)))),]
 
     l1$geometry[which.min(st_distance(trueStart, l1))] = trueStart$geometry
     l2$geometry[which.min(st_distance(trueStart, l2))] = trueStart$geometry
@@ -31,51 +52,32 @@ compare_paths = function(gps, same_paths, truth){
     l2$geometry[which.min(st_distance(trueEnd, l2))] = trueEnd$geometry
 
     make_lines = function(coords, gps){
-      st_linestring(coords) %>%
+        st_linestring(coords) %>%
         st_sfc() %>% st_sf() %>%
         st_set_crs(st_crs(gps))
     }
 
     int = st_coordinates(rbind(trueStart, trueEnd)) %>% make_lines(gps)
 
-    line1 = make_lines(st_coordinates(l1), gps)
+    line1 = l1  %>% st_coordinates() %>% make_lines(gps)
 
-    line2 = make_lines(st_coordinates(l2), gps)
-
-    split = function(line, int){
-      int1 = st_difference(line, st_buffer(st_intersection(line,int), dist=.01)) %>% st_cast("LINESTRING")
-
-      polys = list()
-
-      for(i in 1:nrow(int1)){
-        j = st_coordinates(int1[i,])
-        j = rbind(j,j[1,]) %>%
-          list() %>%
-          st_polygon() %>%
-          st_sfc() %>%
-          st_sf() %>%
-          st_set_crs(st_crs(line)) %>%
-          st_zm()
-
-        polys[[i]] = j
-      }
-
-      do.call(rbind, polys)
-    }
-
-    u = rbind(split(line1, int), split(line2, int))
-
-    u = u[st_is_valid(u),] %>%
-      st_buffer(0) %>%
-      st_intersection() %>%
-      filter(n.overlaps == 1) %>%
-      st_set_crs(st_crs(gps)) %>%
-      st_union() %>%
-      st_sf() %>%
-      st_zm()
+    line2 = l2  %>% st_coordinates() %>% make_lines(gps)
 
 
-    u = st_difference(u, st_buffer(int, .01))
+    ppp = rbind(st_cast(line1,"POLYGON"), st_cast(line2,"POLYGON")) %>% lwgeom::st_make_valid()
+
+    u = ppp %>% st_intersection()  %>% filter(n.overlaps == 1) %>%
+        st_set_crs(st_crs(gps)) %>%
+        st_union() %>%
+        st_sf() %>%
+        st_zm()
+
+    u = st_cast(u, 'POLYGON') %>% mutate(a = st_area(.), feild = 1) #%>% filter(as.numeric(a) > 20)
+
+    u =  u %>%
+      group_by(feild) %>%
+      summarise() %>%
+      st_cast()
 
     if(st_geometry_type(u) == "GEOMETRYCOLLECTION") {
       u = u %>%
